545211215f5f41f29643ee9fd32ef618661f35f1
Sibasis Rath <sibasisrath2000@gmail.com>
2024-08-03 14:37:01 +0530

    Bullet pool to is generic now

--- Assets/Scripts/Bullets/BulletPool.cs
+++ Assets/Scripts/Bullets/BulletPool.cs
@@ -1,3 +1,4 @@
+using CosmicCuration.Utilities;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,53 +10,20 @@ namespace CosmicCuration.Bullets
     // 3. Create a constructor of pool
     // 4. Get a constructor from pool
     // 5. Return a constructor to pool
-    public class BulletPool
+    public class BulletPool : GenericObjectPool<BulletController>
     {
         private BulletView bulletView;
         private BulletScriptableObject bulletScriptableObject;
-
-        private List<PooledBullet> pooledBullets = new List<PooledBullet>();
-
 
         public BulletPool(BulletView bulletView, BulletScriptableObject bulletScriptableObject) 
         {
             this.bulletView = bulletView;
             this.bulletScriptableObject = bulletScriptableObject;
         }
-        public class PooledBullet
-        {
-            public bool isUsed;
-            public BulletController bullet;
-        }
 
-        public BulletController GetBullet()
-        {
-            if (pooledBullets.Count > 0)
-            {
-                PooledBullet pooledBullet = pooledBullets.Find(item => !item.isUsed);
-                if (pooledBullet != null) 
-                {
-                    pooledBullet.isUsed = true;
-                    return pooledBullet.bullet;
-                }                
-            }
-            return CreateNewPooledBullet();
-        }
+        public BulletController GetBullet() => GetItem();
 
-        public void ReturnBullet( BulletController bulletController)
-        {
-            PooledBullet pooledBullet = pooledBullets.Find(item => item.bullet == bulletController);
-            pooledBullet.isUsed = false;
-        }
-
-        private BulletController CreateNewPooledBullet()
-        {
-            PooledBullet pooledBullet = new ();
-            pooledBullet.bullet = new (bulletView, bulletScriptableObject);
-            pooledBullet.isUsed = true;
-            pooledBullets.Add(pooledBullet);
-            return pooledBullet.bullet;
-        }
+        protected override BulletController CreateItem() => new (this.bulletView, this.bulletScriptableObject);
     }
 
 }
