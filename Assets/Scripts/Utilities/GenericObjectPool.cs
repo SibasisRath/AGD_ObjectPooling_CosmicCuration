d30dd862b82324db2955b683e843b4e3bd2d2679
Sibasis Rath <sibasisrath2000@gmail.com>
2024-08-03 13:56:12 +0530

    Generic pool script done

---
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CosmicCuration.Utilities
+{
+    public class GenericObjectPool<T> where T : class
+    {
+        private List<PooledItem<T>> pooledItems = new ();
+
+        protected T GetItem()
+        {
+            if (pooledItems.Count > 0)
+            {
+                PooledItem<T> item = pooledItems.Find(item => !item.isUsed);
+                if (item != null)
+                {
+                    item.isUsed = true;
+                    return item.item;
+                }
+            }
+            return CreatePooledItem();
+        }
+
+        private T CreatePooledItem()
+        {
+            PooledItem<T> newItem = new PooledItem<T>();
+            newItem.item = CreateItem();
+            newItem.isUsed = true;
+            pooledItems.Add(newItem);
+            return newItem.item;
+        }
+
+        protected virtual T CreateItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        public class PooledItem<T>
+        {
+            public T item;
+            public bool isUsed;
+        }
+    }
+}
+
